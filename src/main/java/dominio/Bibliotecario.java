e4b1c86d967a218f17562d5b54223ca0e6057027
Gerlin Orlando Torres Saavedra <gtorress@everis.com>
2017-07-31 17:56:55 -0500

    feat: Ideas de Negocios 1,2 y 3

---
+++ src/main/java/dominio/Bibliotecario.java
@@ -0,0 +1,45 @@
+package dominio;
+
+import dominio.excepcion.PrestamoException;
+import dominio.repositorio.RepositorioLibro;
+import dominio.repositorio.RepositorioPrestamo;
+
+public class Bibliotecario {
+
+	public static final String EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE = "El libro no se encuentra disponible";
+	public static final String EL_LIBRO_SE_UTILIZA_EN_BIBLIOTECA = "los libros palíndromos solo se pueden utilizar en la biblioteca";
+
+	private RepositorioLibro repositorioLibro;
+	private RepositorioPrestamo repositorioPrestamo;
+
+	public Bibliotecario(RepositorioLibro repositorioLibro, RepositorioPrestamo repositorioPrestamo) {
+		this.repositorioLibro = repositorioLibro;
+		this.repositorioPrestamo = repositorioPrestamo;
+
+	}
+
+	public void prestar(String isbn) {
+		if(esPalindromo(isbn)) {
+			throw new PrestamoException(EL_LIBRO_SE_UTILIZA_EN_BIBLIOTECA);
+		}
+		//throw new UnsupportedOperationException("Método pendiente por implementar");
+
+	}
+
+	public boolean esPrestado(String isbn) {
+		Libro libro = this.repositorioPrestamo.obtenerLibroPrestadoPorIsbn(isbn);
+		return null != libro ? true : false;
+	}
+
+	public boolean esPalindromo(String isbn) {
+		int cantidadCaracteres = isbn.length();
+		if (0 == (cantidadCaracteres % 2)) {
+			return new StringBuilder(isbn.substring(cantidadCaracteres / 2, isbn.length())).reverse().toString()
+					.equals(isbn.substring(0, (cantidadCaracteres / 2)));
+		} else {
+			return new StringBuilder(isbn.substring((cantidadCaracteres / 2) + 1, isbn.length())).reverse().toString()
+					.equals(isbn.substring(0, (cantidadCaracteres / 2)));
+		}
+	}
+
+}
---
+++ src/test/java/dominio/unitaria/BibliotecarioTest.java
@@ -0,0 +1,106 @@
+package dominio.unitaria;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+
+import dominio.Bibliotecario;
+import dominio.Libro;
+import dominio.repositorio.RepositorioLibro;
+import dominio.repositorio.RepositorioPrestamo;
+import testdatabuilder.LibroTestDataBuilder;
+
+public class BibliotecarioTest {
+
+	@Test
+	public void esPrestadoTest() {
+		
+		// arrange
+		LibroTestDataBuilder libroTestDataBuilder = new LibroTestDataBuilder();
+		
+		Libro libro = libroTestDataBuilder.build(); 
+		
+		RepositorioPrestamo repositorioPrestamo = mock(RepositorioPrestamo.class);
+		RepositorioLibro repositorioLibro = mock(RepositorioLibro.class);
+		
+		when(repositorioPrestamo.obtenerLibroPrestadoPorIsbn(libro.getIsbn())).thenReturn(libro);
+		
+		Bibliotecario bibliotecario = new Bibliotecario(repositorioLibro, repositorioPrestamo);
+		
+		// act 
+		boolean esPrestado =  bibliotecario.esPrestado(libro.getIsbn());
+		
+		//assert
+		assertTrue(esPrestado);
+	}
+	
+	
+	@Test
+	public void libroNoPrestadoTest() {
+		
+		// arrange
+		LibroTestDataBuilder libroTestDataBuilder = new LibroTestDataBuilder();
+		
+		Libro libro = libroTestDataBuilder.build(); 
+		
+		RepositorioPrestamo repositorioPrestamo = mock(RepositorioPrestamo.class);
+		RepositorioLibro repositorioLibro = mock(RepositorioLibro.class);
+		
+		when(repositorioPrestamo.obtenerLibroPrestadoPorIsbn(libro.getIsbn())).thenReturn(null);
+		
+		Bibliotecario bibliotecario = new Bibliotecario(repositorioLibro, repositorioPrestamo);
+		
+		// act 
+		boolean esPrestado =  bibliotecario.esPrestado(libro.getIsbn());
+		
+		//assert
+		assertFalse(esPrestado);
+	}
+	
+	@Test
+	public void esPalindromoIsbnTest() {
+		
+		// arrange
+		LibroTestDataBuilder libroTestDataBuilder = new LibroTestDataBuilder();
+		
+		Libro libro = libroTestDataBuilder.conIsbn("12321").build(); 
+		
+		RepositorioPrestamo repositorioPrestamo = mock(RepositorioPrestamo.class);
+		RepositorioLibro repositorioLibro = mock(RepositorioLibro.class);
+		
+		when(repositorioPrestamo.obtenerLibroPrestadoPorIsbn(libro.getIsbn())).thenReturn(libro);
+		
+		Bibliotecario bibliotecario = new Bibliotecario(repositorioLibro, repositorioPrestamo);
+		
+		// act 
+		boolean esPalindromo =  bibliotecario.esPalindromo(libro.getIsbn());
+		
+		//assert
+		assertTrue(esPalindromo);
+	}
+	
+	@Test
+	public void noEsPalindromoIsbnTest() {
+		
+		// arrange
+		LibroTestDataBuilder libroTestDataBuilder = new LibroTestDataBuilder();
+		
+		Libro libro = libroTestDataBuilder.build(); 
+		
+		RepositorioPrestamo repositorioPrestamo = mock(RepositorioPrestamo.class);
+		RepositorioLibro repositorioLibro = mock(RepositorioLibro.class);
+		
+		when(repositorioPrestamo.obtenerLibroPrestadoPorIsbn(libro.getIsbn())).thenReturn(libro);
+		
+		Bibliotecario bibliotecario = new Bibliotecario(repositorioLibro, repositorioPrestamo);
+		
+		// act 
+		boolean esPalindromo =  bibliotecario.esPalindromo(libro.getIsbn());
+		
+		//assert
+		assertFalse(esPalindromo);
+	}
+}
